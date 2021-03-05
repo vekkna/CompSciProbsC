2ebaf30203e62d18d0acd272d096576109d02ab4
Neil Crowley <neilccrowley@gmail.com>
2021-03-05 23:20:53 +0000

    added Encryption

--- CompSciProbsC/Encryption.cs
+++ CompSciProbsC/Encryption.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Text;
+using System.Linq;
+
 namespace CompSciProbsC
 {
     class Encryption
     {
+        private static byte[] RandomKey(int length)
+        {
+            var dummy = new byte[length];
+            var random = new Random();
+            random.NextBytes(dummy);
+            return dummy;
+        }
 
+        public static (byte[], byte[]) Encrypt(string original)
+        {
+            var originalBytes = Encoding.UTF8.GetBytes(original);
+            var dummyKey = RandomKey(originalBytes.Length);
+            var encryptedMessage = originalBytes.Zip(dummyKey, (a, b) => (byte)(a ^ b)).ToArray();
+            return (dummyKey, encryptedMessage);
+        }
+
+        public static string Decrypt(byte[] dummyKey, byte[] encryptedMessage)
+        {
+            var decryptedMessage = encryptedMessage.Zip(dummyKey, (a, b) => (byte)(a ^ b)).ToArray();
+            return Encoding.UTF8.GetString(decryptedMessage);
+        }
     }
 }
